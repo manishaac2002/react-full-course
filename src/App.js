afae18f9e93f193cd5e2675e8ad8cbf06c64587b
manishaac2002 <manishaac2002@gmail.com>
2024-07-03 12:17:05 +0530

    feat: Click events.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 function App() {
-
+                                                                        
 
   return (
     <div >
--- src/Content.js
+++ src/Content.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 const Content = () => {
 
+    const handleClick = (e) => {
+        console.log(e.target.innerText)
+    }
+    const handleClick2 = (names) => {
+        console.log(`Thanks for purchasing course ${names}`)
+    }
+
     function handleChangeName() {
         const programmingLanguage = ["Javascript", "React.js", "MongoDB", "Nodejs", "Express.js"]
         const int = Math.floor(Math.random() * 4)
@@ -11,8 +18,16 @@ const Content = () => {
     return (
 
         <main>
-            Welcome all...!
-            <p>Let's learn {handleChangeName()}.</p>
+            <p>
+                <h3 onDoubleClick={() => handleClick2("Manisha")}>
+                    Welcome all...!</h3>
+                Let's learn {handleChangeName()}.
+
+                <button onClick= {(e) => handleClick(e)}>
+                    Purchase course
+                </button>
+
+            </p>
         </main >
 
     )
